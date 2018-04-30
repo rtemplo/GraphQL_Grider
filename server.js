2dde140614868023b4c19e94316e803421152d80
Ray Templo <rtemplo@gmail.com>
2018-04-30 11:25:13 -0400

    using lodash for hard coded test user data

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
+
+var PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log('Listening on port',  PORT);
+});
